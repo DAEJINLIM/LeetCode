3b6eaaffa9abac63617594607ffbddf2c0cdc672
DAEJINLIM <115560272+DAEJINLIM@users.noreply.github.com>
2026-05-07 23:41:11 +0900

    Time: 0 ms (100%), Space: 21 MB (40.61%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.swift
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.swift
@@ -1,23 +1,13 @@
 class Solution {
     func maxProfit(_ prices: [Int]) -> Int {
-        var arr = [(Int,Int)]()
-        var low = 1000000
-        var high = 0
-
-        for i in prices {
-            if low > i {
-                if high != 0 {
-                    arr.append((low,high))
-                }
-                high = 0
-                low = i
-            } else if high < i {
-                high = i
-            }
+        var minPrice = Int.max
+        var profit = 0
+        
+        for price in prices {
+            minPrice = min(minPrice, price)
+            profit = max(profit, price - minPrice)
         }
-        if high != 0 {
-            arr.append((low,high))
-        }
-        return arr.count == 0 ? 0 : arr.map { $0.1 - $0.0 }.max()!
+        
+        return profit
     }
 }
