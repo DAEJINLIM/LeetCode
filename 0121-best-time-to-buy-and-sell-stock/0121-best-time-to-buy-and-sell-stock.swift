c97408930db7aa3dfdd68937c8dda284ec6ce5e2
DAEJINLIM <115560272+DAEJINLIM@users.noreply.github.com>
2026-05-07 23:36:43 +0900

    Time: 211 ms (5.8%), Space: 21.3 MB (24.72%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.swift
@@ -0,0 +1,25 @@
+class Solution {
+    func maxProfit(_ prices: [Int]) -> Int {
+        var arr = [(Int,Int)]()
+        var low = 1000000
+        var high = 0
+
+        for i in prices {
+            if low > i {
+                if high != 0 {
+                    arr.append((low,high))
+                }
+                high = 0
+                low = i
+            } else if high < i {
+                high = i
+            }
+        }
+        if high != 0 {
+            arr.append((low,high))
+        }
+        return arr.count == 0 ? 0 : arr.map { $0.1 - $0.0 }.max()!
+    }
+}
+
+Solution().maxProfit([7,1,5,3,6,4])
