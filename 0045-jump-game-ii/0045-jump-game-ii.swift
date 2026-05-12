a50d426d38db4e4396c01c923bd274406ca85e53
DAEJINLIM <115560272+DAEJINLIM@users.noreply.github.com>
2026-05-13 01:20:43 +0900

    Time: 0 ms (100%), Space: 20.1 MB (31.01%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.swift
+++ 0045-jump-game-ii/0045-jump-game-ii.swift
@@ -1,15 +1,15 @@
 class Solution {
     func jump(_ nums: [Int]) -> Int {
         var jumps = 0
-        var currentEnd = 0
+        var jumpIndex = 0
         var farthest = 0
 
         for i in 0..<nums.count-1 {
             farthest = max(farthest, i + nums[i])
-
-            if i == currentEnd {
+            
+            if i == jumpIndex {
                 jumps += 1
-                currentEnd = farthest
+                jumpIndex = farthest
             }
         }
         
