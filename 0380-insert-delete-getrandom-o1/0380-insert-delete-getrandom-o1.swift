d0a61da1414abcafe13042c32674067f536b716d
DAEJINLIM <115560272+DAEJINLIM@users.noreply.github.com>
2026-05-15 00:04:26 +0900

    Time: 108 ms (84.24%), Space: 33 MB (87.68%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.swift
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.swift
@@ -1,20 +1,34 @@
-
 class RandomizedSet {
-    var arr = Set<Int>()
+    var arr = [Int]()
+    var dic = [Int: Int]()
 
     init() {
     }
     
     func insert(_ val: Int) -> Bool {
-        let count = arr.count
-        arr.insert(val)
-        return count == arr.count ? false : true
+        if let _ = dic[val] {
+            return false
+        } else {
+            dic[val] = arr.count
+            arr.append(val)
+            return true
+        }
     }
     
     func remove(_ val: Int) -> Bool {
-        let count = arr.count
-        arr.remove(val)
-        return count == arr.count ? false : true
+        if let index = dic[val] {
+            let last = arr.last!
+
+            arr[index] = last
+            dic[last] = index
+
+            arr.removeLast()
+            dic.removeValue(forKey: val)
+
+            return true
+        } else {
+            return false
+        }
     }
     
     func getRandom() -> Int {
