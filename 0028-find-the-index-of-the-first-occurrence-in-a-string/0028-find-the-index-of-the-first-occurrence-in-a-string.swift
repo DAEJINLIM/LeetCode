4f9d51e1bc28c443ac8a01243158888487b94043
DAEJINLIM <115560272+DAEJINLIM@users.noreply.github.com>
2026-06-02 02:02:35 +0900

    Time: 0 ms (100%), Space: 19.3 MB (98.25%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.swift
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.swift
@@ -2,30 +2,18 @@ class Solution {
     func strStr(_ haystack: String, _ needle: String) -> Int {
         let h = Array(haystack)
         let n = Array(needle)
-
         
-
         if h.count < n.count { return -1 }
-
-
+        
         for i in 0...(h.count - n.count) {
-
             var found = true
-
             for j in 0..<n.count {
-
                 if h[i + j] != n[j] {
-
                     found = false
-
                     break
-
                 }
-
             }
-
             if found { return i }
-
         }
 
         return -1
