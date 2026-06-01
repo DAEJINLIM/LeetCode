31848a8095801033c5eecf07af05aa34c6218276
DAEJINLIM <115560272+DAEJINLIM@users.noreply.github.com>
2026-06-02 00:57:36 +0900

    Time: 0 ms (100%), Space: 19.7 MB (74.83%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.swift
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.swift
@@ -3,18 +3,29 @@ class Solution {
         let h = Array(haystack)
         let n = Array(needle)
 
-        if n.isEmpty { return 0 }
-        if h.count < n.count { return -1 }
         
+
+        if h.count < n.count { return -1 }
+
+
         for i in 0...(h.count - n.count) {
+
             var found = true
+
             for j in 0..<n.count {
+
                 if h[i + j] != n[j] {
+
                     found = false
+
                     break
+
                 }
+
             }
+
             if found { return i }
+
         }
 
         return -1
