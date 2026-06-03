77cf01ae23547efbc867d787c36c0dee3f5d58e5
DAEJINLIM <115560272+DAEJINLIM@users.noreply.github.com>
2026-06-04 00:14:52 +0900

    Time: 0 ms (100%), Space: 20.4 MB (15.38%) - LeetHub

--- 0068-text-justification/0068-text-justification.swift
+++ 0068-text-justification/0068-text-justification.swift
@@ -2,21 +2,22 @@ class Solution {
     func fullJustify(_ words: [String], _ maxWidth: Int) -> [String] {
         var result = [String]()
         var text = [String]()
+        var charCount = 0
 
         for word in words {
-            let reduce = text.map { $0.count }.reduce(0, +)
-
-            if reduce + word.count + text.count <= maxWidth {
+            if charCount + word.count + text.count <= maxWidth {
                 text.append(word)
+                charCount += word.count
             } else {
                 var index = 0
-                for _ in 0..<(maxWidth -  reduce) {
+                for _ in 0..<(maxWidth -  charCount) {
                     text[index] += " "
                     index += 1
                     index %= max(text.count - 1, 1)
                 }
                 result.append(text.joined())
                 text = [word]
+                charCount = word.count
             }
         }
 
