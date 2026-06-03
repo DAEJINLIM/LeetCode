5a42f631836bfd2c09abc17104a9e18521c278fa
DAEJINLIM <115560272+DAEJINLIM@users.noreply.github.com>
2026-06-04 01:20:15 +0900

    Time: 16 ms (33.13%), Space: 20.7 MB (40.17%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.swift
+++ 0125-valid-palindrome/0125-valid-palindrome.swift
@@ -1,6 +1,6 @@
 class Solution {
     func isPalindrome(_ s: String) -> Bool {
-        let s = s.filter { $0.isLetter || $0.isNumber }.map { $0.lowercased() }
+        let s = Array(s.lowercased().filter { $0.isLetter || $0.isNumber })
         var left = 0
         var right = s.count - 1
 
