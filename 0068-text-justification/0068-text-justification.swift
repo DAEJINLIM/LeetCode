e633ea02a0fde12c77e029bccd6707c19937f659
DAEJINLIM <115560272+DAEJINLIM@users.noreply.github.com>
2026-06-04 00:15:42 +0900

    Time: 0 ms (100%), Space: 20.2 MB (29.49%) - LeetHub

--- 0068-text-justification/0068-text-justification.swift
+++ 0068-text-justification/0068-text-justification.swift
@@ -1,27 +1,27 @@
 class Solution {
     func fullJustify(_ words: [String], _ maxWidth: Int) -> [String] {
         var result = [String]()
-        var text = [String]()
+        var line = [String]()
         var charCount = 0
 
         for word in words {
-            if charCount + word.count + text.count <= maxWidth {
-                text.append(word)
+            if charCount + word.count + line.count <= maxWidth {
+                line.append(word)
                 charCount += word.count
             } else {
                 var index = 0
                 for _ in 0..<(maxWidth -  charCount) {
-                    text[index] += " "
+                    line[index] += " "
                     index += 1
-                    index %= max(text.count - 1, 1)
+                    index %= max(line.count - 1, 1)
                 }
-                result.append(text.joined())
-                text = [word]
+                result.append(line.joined())
+                line = [word]
                 charCount = word.count
             }
         }
 
-        let last = text.joined(separator: " ")
+        let last = line.joined(separator: " ")
         result.append(last + String(repeating: " ", count: maxWidth - last.count))
 
         return result
