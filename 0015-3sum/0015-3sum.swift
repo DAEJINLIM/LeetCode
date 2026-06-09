3ee658e2b7ce522fa3a24ddfc45a1ac7927454f0
DAEJINLIM <115560272+DAEJINLIM@users.noreply.github.com>
2026-06-09 23:44:29 +0900

    Time: 35 ms (39.35%), Space: 22.4 MB (67.16%) - LeetHub

--- 0015-3sum/0015-3sum.swift
+++ 0015-3sum/0015-3sum.swift
@@ -1,9 +1,15 @@
 class Solution {
     func threeSum(_ nums: [Int]) -> [[Int]] {
         let nums = nums.sorted()
-        var arr = Set<[Int]>()
+        var arr = [[Int]]()
 
         for i in 0..<nums.count {
+
+            if i > 0 && nums[i] == nums[i - 1] {
+
+                continue
+
+            }
             var l = i + 1
             var r = nums.count - 1
 
@@ -11,20 +17,18 @@ class Solution {
                 let sum = nums[i] + nums[l] + nums[r]
                 
                 if sum == 0 {
-                    arr.insert([nums[i], nums[l], nums[r]])
+                    arr.append([nums[i], nums[l], nums[r]])
                     l += 1
                     r -= 1
+
                     while l < r && nums[l] == nums[l - 1] {
-
                         l += 1
-
                     }
 
                     while l < r && nums[r] == nums[r + 1] {
-
                         r -= 1
-
                     }
+
                 } else if sum < 0 {
                     l += 1
                 } else {
@@ -33,6 +37,6 @@ class Solution {
             }
         }
         
-        return Array(arr)
+        return arr
     }
 }
