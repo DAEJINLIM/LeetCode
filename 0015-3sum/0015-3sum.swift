597cc1a5a1f360eea9ba5886f65f0ec0725a810b
DAEJINLIM <115560272+DAEJINLIM@users.noreply.github.com>
2026-06-09 23:42:42 +0900

    Time: 40 ms (38.13%), Space: 23.3 MB (5.7%) - LeetHub

--- 0015-3sum/0015-3sum.swift
+++ 0015-3sum/0015-3sum.swift
@@ -14,7 +14,17 @@ class Solution {
                     arr.insert([nums[i], nums[l], nums[r]])
                     l += 1
                     r -= 1
+                    while l < r && nums[l] == nums[l - 1] {
 
+                        l += 1
+
+                    }
+
+                    while l < r && nums[r] == nums[r + 1] {
+
+                        r -= 1
+
+                    }
                 } else if sum < 0 {
                     l += 1
                 } else {
