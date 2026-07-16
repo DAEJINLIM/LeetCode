07d2c9aac49b01f28f951fe6264cea7759e68ed0
DAEJINLIM <115560272+DAEJINLIM@users.noreply.github.com>
2026-07-17 01:38:05 +0900

    Time: 0 ms (100%), Space: 19.5 MB (84.29%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.swift
+++ 0290-word-pattern/0290-word-pattern.swift
@@ -1,28 +1,27 @@
 class Solution {
     func wordPattern(_ pattern: String, _ s: String) -> Bool {
-        let p = pattern
         let words = s.split(separator: " ").map { String($0) }
 
-        if p.count != words.count { return false }
+        if pattern.count != words.count { return false }
 
         var dicP = [Character: String]()
         var dicWords = [String: Character]()
 
-        for (a, b) in zip(p, words) {
-            if let value1 = dicP[a] {
-                if value1 != b {
+        for (p, w) in zip(pattern, words) {
+            if let value1 = dicP[p] {
+                if value1 != w {
                     return false
                 }
             } else {
-                dicP[a] = b
+                dicP[p] = w
             }
 
-            if let value2 = dicWords[b] {
-                if value2 != a {
+            if let value2 = dicWords[w] {
+                if value2 != p {
                     return false
                 }
             } else {
-                dicWords[b] = a
+                dicWords[w] = p
             }
         }
         
