0c39d1ca506bd91a1adb41227937b686b90e9bc0
DAEJINLIM <115560272+DAEJINLIM@users.noreply.github.com>
2026-07-28 01:59:10 +0900

    Time: 0 ms (100%), Space: 20.1 MB (5%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.swift
+++ 0228-summary-ranges/0228-summary-ranges.swift
@@ -1,27 +1,20 @@
 class Solution {
     func summaryRanges(_ nums: [Int]) -> [String] {
-        guard nums.count != 0 else { return [] }
-        guard nums.count != 1 else { return [String(nums.first!)] }
+        guard !nums.isEmpty else { return [] }
+
         var result = [String]()
-        var startNum = nums.first!
-        var lastNum = nums.first!
+        var start = nums.first!
 
-        for i in 1..<nums.count {
-            if lastNum + 1 != nums[i] {
-                if startNum == lastNum {
-                    result.append("\(lastNum)")
+        for i in 1...nums.count {
+            if i == nums.count || nums[i] != nums[i - 1] + 1 {
+                if start == nums[i - 1] {
+                    result.append("\(start)")
                 } else {
-                    result.append("\(startNum)->\(lastNum)")
+                    result.append("\(start)->\(nums[i - 1])")
                 }
-                startNum = nums[i]
-            }
-            lastNum = nums[i]
 
-            if i == nums.count-1 {
-                if startNum == lastNum {
-                    result.append("\(lastNum)")
-                } else {
-                    result.append("\(startNum)->\(lastNum)")
+                if i < nums.count {
+                    start = nums[i]
                 }
             }
         }
