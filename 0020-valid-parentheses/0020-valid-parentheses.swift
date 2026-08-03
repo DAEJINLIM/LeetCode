c8b45934080e567168111aa433800cf15701567b
DAEJINLIM <115560272+DAEJINLIM@users.noreply.github.com>
2026-08-03 23:02:19 +0900

    Time: 0 ms (100%), Space: 19.6 MB (93.08%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.swift
@@ -0,0 +1,22 @@
+class Solution {
+    func isValid(_ s: String) -> Bool {
+        if s.count % 2 == 1 { return false }
+
+        var str = [Character]()
+
+        for i in s {
+            if i == "(" || i == "{" || i == "[" {
+                str.append(i)
+            } else {
+                if str.count == 0 { return false }
+                
+                let last = str.removeLast()
+                if i == ")" && last != "(" || i == "}" && last != "{"  || i == "]" && last != "[" {
+                    return false
+                }
+            }
+        }
+
+        return str.count > 0 ? false : true
+     }
+}
