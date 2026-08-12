09eb85c966397cabaca28c53c3d35f26f2710b00
DAEJINLIM <115560272+DAEJINLIM@users.noreply.github.com>
2026-08-13 04:16:44 +0900

    Time: 0 ms (100%), Space: 19.5 MB (57.88%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.swift
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.swift
@@ -12,10 +12,10 @@ class Solution {
     func mergeTwoLists(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
         var node1 = list1
         var node2 = list2
-        
-        let dummy = ListNode(0)
-        var current = dummy
-        
+
+        let start = ListNode(0)
+        var current = start
+
         while node1 != nil && node2 != nil {
             if node1!.val <= node2!.val {
                 current.next = node1
@@ -24,16 +24,16 @@ class Solution {
                 current.next = node2
                 node2 = node2?.next
             }
-            
+
             current = current.next!
         }
-        
+
         if node1 != nil {
             current.next = node1
         } else {
             current.next = node2
         }
-        
-        return dummy.next
+
+        return start.next
     }
 }
